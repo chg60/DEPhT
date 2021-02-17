8c0d6e93b949088113597b2962580eb231d9f96c
ananya-venbakkam <ananya.venbakkam@gmail.com>
2021-02-17 13:03:20 -0500

    parallelization complete

--- wrappers/hhsearch_wrapper.py
+++ wrappers/hhsearch_wrapper.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
-import argparse
-import subprocess
+from multiprocessing import cpu_count
 
-def wrapper(args):
+from pdm_utils.functions.parallelize import *
+
+import argparse
+from subprocess import Popen, PIPE
+
+def wrapper(input_file, output_dir, database, cutoff):
+
+    # take in three arguments instead
 
     """
     Wrapper for HHSearch
@@ -11,14 +17,42 @@ def wrapper(args):
     :type: list
     """
 
+    output_file = Path(output_dir/(input_file.stem + ".hhr"))
 
-    input_file = args.input_file
-    database = args.database
-    cutoff = args.cutoff
+    print(output_file)
 
+    output = open(output_file, "w")
 
     # run hhsearch
-    subprocess.run(["hhsearch", "-i", input_file, "-d", database, "-e", cutoff])
+    # subprocess.run(["hhsearch", "-i", input_file, "-d", database, "-e", cutoff])
+
+    # return output file instead of out
+
+    # with Popen(["hhsearch", "-i", input_file, "-d", database, "-e", cutoff], stdout=output, stderr=output) as p:
+        # out = p.stdout.read().decode("utf-8")
+        #err = p.stderr.read().decode("utf-8")
+
+    Popen(["hhsearch", "-i", input_file, "-d", database, "-e", cutoff], stdout=output, stderr=output) 
+
+    return output_file
+
+
+def check_output_dir(output_dir):
+    if output_dir.is_dir() is False:
+        output_dir.mkdir()
+
+    return output_dir
+
+
+def create_job_queue(input_dir, output_dir, database, cutoff):
+
+    jobs = []
+
+    for filepath in input_dir.iterdir():
+        if filepath.suffix == ".fasta":
+            jobs.append((filepath, output_dir, database, cutoff))
+
+    return jobs
 
 def parse_args():
 
@@ -27,12 +61,18 @@ def parse_args():
     :returns: list containing parsed arguments
     """
 
-    input_help = "Path to the input file"
+    # ask for ouput, make sure it exists, stem of input for each - .hhr extension for output file
+    # create if it doesn't
+    # run subprocess with -o flag
+
+    input_help = "Path to the input directory"
+    output_help = "Path to the output directory"
     db_help = "Path to the database being searched against"
     cutoff_help = "E-value cutoff for results"
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("input_file", type=Path, help=input_help)
+    parser.add_argument("input_dir", type=Path, help=input_help)
+    parser.add_argument("output_dir", type=Path, help=output_help)
     parser.add_argument("database", type=Path, help=db_help)
     parser.add_argument("-c", "--cutoff", type=str, default="0.001", help=cutoff_help)
     args = parser.parse_args()
@@ -48,7 +88,13 @@ def main():
     args = parse_args()
 
     print(args)
-    wrapper(args)
+
+    output_dir = check_output_dir(args.output_dir)
+
+    jobs = create_job_queue(args.input_dir, output_dir, args.database, args.cutoff)
+
+    results = parallelize(jobs, cpu_count()//2, wrapper) # list of output filepaths
+    print(results)
 
 
 
