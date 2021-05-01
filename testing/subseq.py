96496609e12f313a28b236f6ca8514d9a76c93ff
ananya-venbakkam <ananya.venbakkam@gmail.com>
2021-05-01 12:48:45 -0400

    debugged

--- testing/subseq.py
+++ testing/subseq.py
@@ -1,52 +1,57 @@
 from Bio import SeqIO
 from pathlib import Path
 
+import csv
 import argparse
 import sys
-# import time
 
 
 # Global variables
 CUT_OFF = 0.5
+CSV_HEADERS = ["strain", "software", "prophage", "ends"]
+GENOME_LENGTHS = {}
 
 
 # Will convert the file to a biopython object
-def open_file(filename):
+def open_file(filename, store_length=False):
+    """Convert the FASTA formatted file into a biopython object.
 
-    # print(filename)
-
-    """
-    Converts the FASTA formatted file into a biopython object
     :param: filename
     :type: Path
     :return: SeqRecord object generated from the FASTA file
     :rtype: Bio.SeqRecord.SeqRecord
     """
-
-    # print(filename)
-
-    # Open file
-
-    '''
-    with open(filename, "r") as fh:
-        # Convert file to a biopython seq object
-        # use SeqIO.parse
-        # only take in longest sequence!
-        record = SeqIO.read(fh, "fasta")
-    return record
-    '''
-
+    # instead iterate through all the contigs
     contigs = [record for record in SeqIO.parse(filename, "fasta")]
-    contig_lengths = [len(record) for record in contigs]
-    longest_contig = contigs[contig_lengths.index(max(contig_lengths))]
+    # contig_lengths = [len(record) for record in contigs]
+    # longest_contig = contigs[contig_lengths.index(max(contig_lengths))]
 
-    return longest_contig
+    length = 0
+    for record in contigs:
+        length += len(record)
+
+    if store_length:
+        GENOME_LENGTHS[filename.stem] = length
+
+    return contigs
 
 
-def find_sub(parent, other):
+def get_genome_length(contigs):
+    """Return the length of the genome.
 
+    :param contigs: list of contigs in the fasta file
+    :type contigs: list
     """
-    Locates the prophage region as a subsequence of the bacterial genome
+    length = 0
+    for record in contigs:
+        length += len(record)
+
+    return length
+
+
+def find_sub(parent, child_contigs):
+    """Locate the prophage region as a subsequence of the bacterial genome.
+
     :param parent: Parent sequence
     :type parent: Bio.SeqRecord.SeqRecord
     :param other: Subsequence being located in the parent
@@ -54,247 +59,275 @@ def find_sub(parent, other):
     :return: left end of the subsequence
     :rtype: int
     """
+    # keep track of which contig it is in
 
-    # Find the beginning of the subsequence
-    left_coor = parent.seq.find(other.seq)
-    # Checks reverse complement
-    if left_coor == -1:
-        reverse = other.reverse_complement()
-        left_coor = parent.seq.find(reverse.seq)
-        # if nothing is returned an error exists
+    for record in child_contigs:
+        # Find the beginning of the subsequence
+        left_coor = parent.seq.find(record.seq)
+        # Checks reverse complement
         if left_coor == -1:
-            return None
+            reverse = record.reverse_complement()
+            left_coor = parent.seq.find(reverse.seq)
+            # if nothing is returned an error exists
+            if left_coor == -1:
+                return None
     return left_coor
 
 
-def comparison(manual_data, testing_data):
-    # testing_data format
-    # {phage_name: {software_name: {prophage_name: <ends>, ...}, ...}, ...}
-    # manual_data format
-    # {phage_name: {prophage_name: <ends>, ...}, ...}
+def stats(manual_data, testing_data):
+    """Return True Positive, False Positive, True Negative, False Negative.
 
-    # go through both dictionaries
-    # compare each testing data ends tuple with every manually annotated one
-
-    matches = {}
-
-    for phage in testing_data:
-        # list of all prophage ends
-        matches[phage] = {}
-        for software in testing_data[phage]:
-            ends_list = list(testing_data[phage][software].values())
-
-            matches[phage][software] = []
-
-            for ends_tuple in ends_list:
-                manual_ends = list(manual_data[phage].values())
-                ind = ends_list.index(ends_tuple)
-                proph_name = list(testing_data[phage][software].keys())[ind]
-
-                for manual_ends_tuple in manual_ends:
-                    binary = compare_ends(manual_ends_tuple, ends_tuple)
-
-                    if binary is True:
-                        matches[phage][software].append(proph_name)
-
-    return matches
-
-
-def compare_ends(manual_coor, test_coor):
+    :param manual_coordinates: Manual prophage names w/ Coordinates
+    :type manual_coordinates: dict
+    :param software_coordinates: Software prophages names w/ Coordinates
+    :type software_coordinates: dict
+    :return: True Positive, False Positive, True Negative, False Negative
+    :rtype: dict
     """
-    Compares the manually annotated ends with the ends from testing software
-    :param manual_coor: Coordinates from manually annotates genomes
-    :param test_coor: Coordinates from testing software
-    :return: True or False based on whether the overlap is over 50% or not
-    :rtype: bool
+    statistics = {}
+    statistics["TRUE_POSITIVE"] = 0
+    statistics["FALSE_POSITIVE"] = 0
+    statistics["FALSE_NEGATIVE"] = 0
+    manual_set = set()
+    soft_set = set()
+
+    for ends in testing_data.values():  # iterates over the ends
+        ends = eval(ends)
+        for coor in range(int(ends[0]), int(ends[1]) + 1):
+            soft_set.add(coor)
+    for ends in manual_data.values():  # iterates over the ends for manual
+        ends = eval(ends)
+        for coor in range(int(ends[0]), int(ends[1]) + 1):
+            manual_set.add(coor)
+
+    statistics["TRUE_POSITIVE"] += len(
+        soft_set.intersection(manual_set))
+    statistics["FALSE_POSITIVE"] += len(
+        soft_set.difference(manual_set))
+    statistics["FALSE_NEGATIVE"] += len(
+        manual_set.difference(soft_set))
+    return statistics
+
+
+def collect_stats(filepath):
+    """Compile raw bp values of TP,FP,TN,FN.
+
+    :param filepath: filepath of the csv file with data entries
+    :type filepath: Path
+    :return: dictionary containing TP, TN, FP, FN
+    :rtype: dict
     """
+    true_positive = 0  # the bp of true positives
+    true_negative = 0  # the bp of true negatives
+    false_positive = 0  # the bp of false positives
+    false_negative = 0  # the bp of
+    length_manual = 0
+    length_test = 0
+    manual_ends = {}  # {name: (start, stop)}
+    testing_ends = {}
+    statistics = {}
 
-    manual_positions = set()
-    test_positions = set()
+    statistics["TRUE_POSITIVE"] = 0
+    statistics["FALSE_POSITIVE"] = 0
+    statistics["FALSE_NEGATIVE"] = 0
+    statistics["TRUE_NEGATIVE"] = 0
 
-    for i in range(manual_coor[0], manual_coor[1]+1):
-        manual_positions.add(i)
+    with filepath.open(mode="r") as filehandle:
 
-    for i in range(test_coor[0], test_coor[1]+1):
-        test_positions.add(i)
+        csv_reader = csv.reader(filehandle, delimiter=",", quotechar='"')
 
-    inter_set = manual_positions.intersection(test_positions)
+        for strain in GENOME_LENGTHS:
+            for row in csv_reader:
+                if row[1] == "manual" and row[0] == strain:
+                    manual_ends[row[2]] = row[3]
+                    ends_tuple = eval(manual_ends.get(row[2]))
+                    if ends_tuple[1] - ends_tuple[0] < 0:
+                        length_manual += ends_tuple[0] - ends_tuple[1]
+                    else:
+                        length_manual += ends_tuple[1] - ends_tuple[0]
+                if row[1] == filepath.stem.split(
+                        "_")[0] and row[0] == strain:
+                    testing_ends[row[2]] = row[3]
+                    test_ends = eval(testing_ends.get(row[2]))
+                    if test_ends[1] - test_ends[0] < 0:
+                        length_test += test_ends[0] - test_ends[1]
+                    else:
+                        length_test += test_ends[1] - test_ends[0]
 
-    # Overlapping score
-    overlap = len(inter_set)/(manual_coor[1]-manual_coor[0])
+            per_phage = stats(manual_ends, testing_ends)
+            true_positive += per_phage.get("TRUE_POSITIVE")
+            false_positive += per_phage.get("FALSE_POSITIVE")
+            false_negative += per_phage.get("FALSE_NEGATIVE")
 
-    # return True/False
-    return overlap > CUT_OFF
+            true_negative += GENOME_LENGTHS[strain] - \
+                length_manual - length_test - false_negative
+    statistics["TRUE_POSITIVE"] += true_positive
+    statistics["FALSE_POSITIVE"] += false_positive
+    statistics["FALSE_NEGATIVE"] += false_negative
+    statistics["TRUE_NEGATIVE"] += true_negative
+    return statistics
 
 
-def get_dir():
+def metrics(stats):
+    """Calculate the metric data.
+
+    :param stats: statistics data with tp, tn, fn, fp values
+    :type stats: dict
+    :return: Dictionary containing sensitivty, positive predictive value
+             and accuracy
+    :rtype: dict
     """
-    Parses the argument list
+    tp = stats["TRUE_POSITIVE"]
+    tn = stats["TRUE_NEGATIVE"]
+    fn = stats["FALSE_POSITIVE"]
+    fp = stats["FALSE_NEGATIVE"]
+
+    sn = (tp/(tp+fn))*100
+    ppv = (tp/(tp+fp))*100
+    acc = ((tp+tn)/(tp+tn+fp+fn))*100
+
+    return {"sensitivity": round(sn, 2),
+            "positive predictive value": round(ppv, 2),
+            "accuracy": round(acc, 2)}
+
+
+def get_child_ends(prophage_filepath, parent_contigs):
+    """Return the ends of the prophage region as a tuple.
+
+    :param prophage_filepath: Filepath to the hypothetical prophage FASTA
+    :type prophage_filepath: Path
+    :param parent_seq: Sequence of the parent genome
+    :return: Tuple of prophage ends
+    :rtype: tuple
+    """
+    # change so that it reads all the prophages in that folder
+    # child_seq = open_file(prophage_filepath)
+    child_contigs = open_file(prophage_filepath)
+    child_tuples = []
+
+    for child_seq in child_contigs:
+
+        for parent_seq in parent_contigs:
+            start = find_sub(parent_seq, child_contigs)
+
+            if start is None:
+                continue
+
+            child_tuples.append((start, start+len(child_seq)))
+
+    return child_tuples
+
+
+def comparison(dir, manual_name, software_name):
+    """Compare the data.
+
+    :param dir: Path to working directory
+    :type dir: Path
+    :param manual_name: Name of directory with manually annotated genomes
+    :type manual_name: str
+    :param software_name: Name of software being tested
+    :type software_name: str
+    """
+    path_to_manual = dir/"prophages"/manual_name
+    path_to_software = dir/"prophages"/software_name
+
+    genome_path = dir/"genomes"
+
+    csv_filename = software_name + "_testing_results.csv"
+
+    csv_path = dir/csv_filename
+
+    with open(csv_path, mode="w") as csv_file:
+
+        writer = csv.DictWriter(csv_file, fieldnames=CSV_HEADERS)
+        writer.writeheader()
+
+        for genome in sorted(genome_path.iterdir()):
+            strain_name = genome.stem
+            ref_seq = open_file(genome, True)     # reference genome sequence
+            manual_strain_path = path_to_manual/strain_name
+            software_strain_path = path_to_software/strain_name
+
+            for prophage in sorted(manual_strain_path.iterdir()):
+                manual_ends = get_child_ends(prophage, ref_seq)
+                if manual_ends is None:
+                    print("ERROR: Prophage not found: ", prophage)
+                    dict = {"strain": strain_name,
+                            "software": "manual",
+                            "prophage": prophage.stem,
+                            "ends": "NONE"}
+                    writer.writerow(dict)
+                else:
+                    for ends_tuple in manual_ends:
+                        dict = {"strain": strain_name,
+                                "software": "manual",
+                                "prophage": prophage.stem,
+                                "ends": ends_tuple}
+                        writer.writerow(dict)
+
+            for prophage in sorted(software_strain_path.iterdir()):
+                '''
+                try:
+                    software_ends_tuple = get_child_ends(prophage, ref_seq)
+                except UnicodeDecodeError:
+                    # need a better way to handle this - check for fasta
+                    # file (ext)
+                    # or catch error somewhere else??
+                    continue
+                '''
+                # check that no hidden files are being read in
+                if prophage.suffix == ".fasta":
+                    software_ends = get_child_ends(prophage, ref_seq)
+                else:
+                    continue
+
+                if software_ends is None:
+                    dict = {"strain": strain_name,
+                            "software": software_name,
+                            "prophage": prophage.stem,
+                            "ends": "NONE"}
+                    writer.writerow(dict)
+                else:
+                    for ends_tuple in software_ends:
+                        dict = {"strain": strain_name,
+                                "software": software_name,
+                                "prophage": prophage.stem,
+                                "ends": ends_tuple}
+                        writer.writerow(dict)
+        csv_file.close()
+
+        stats = collect_stats(csv_path)
+        print(stats)
+        metric_data = metrics(stats)
+        print(metric_data)
+        # close files!!
+
+
+def get_args():
+    """Parse the argument list.
+
     :return: Working directory where the sequences are located
     :rtype: Path
     """
     parser = argparse.ArgumentParser(description=__doc__)
     dir_help = "Path to working directory"
+    manual_help = "Name of directory with manually annotated prophages"
+    software_help = "Name of the software being tested"
 
     parser.add_argument("dir", type=Path, help=dir_help)
+    parser.add_argument("manual", type=str, help=manual_help)
+    parser.add_argument("software", type=str, help=software_help)
     args = parser.parse_args()
 
-    return args.dir
-
-
-def get_parent_data(name, parent_dir):
-    """
-    Gets the ends of the parent sequence and the sequence contained in
-    the parent FASTA
-    :param name: Name of the parent genome
-    :type name: str
-    :param parent_dir: Directory where the parent FASTA is stored
-    :return: List of two elements - tuple of ends and the parent sequence
-    :rtype: list
-    """
-    parent_file = name + ".fasta"
-    filepath = parent_dir/parent_file
-    parent_seq = open_file(filepath)
-    parent_tuple = (1, len(parent_seq))
-    return [parent_tuple, parent_seq]
-
-
-def get_child_ends(prophage_filepath, parent_seq):
-    """
-    Returns the ends of the prophage region as a tuple
-    :param prophage_filepath: Filepath to the hypothetical prophage FASTA
-    :type prophage_filepath: Path
-    :param parent_seq: Sequence ofthe parent genome
-    :return: Tuple of prophage ends
-    :rtype: tuple
-    """
-
-    # change so that it reads all the prophages in that folder
-    child_seq = open_file(prophage_filepath)
-    start = find_sub(parent_seq, child_seq)
-
-    if start is None:
-        return None
-
-    child_tuple = (start, start+len(child_seq))
-    return child_tuple
-
-
-def get_child_data(child_dir, parent_seq):
-
-    """
-    Returns a dictionary of the ends tuple with prophage name as the key
-    :param child_dir: Child directory with hypothetical prophages
-    :type child_dir: pathlib.Path
-    :param parent_seq: Bacterial sequence
-    :type parent_seq: SeqRecord object
-    :return: Dictionary with prophage name keys and ends tuple values
-    :rtype: dict
-    """
-
-    ends = {}
-
-    for prophage in sorted(child_dir.iterdir()):
-        prophage_name = prophage.stem
-        ends[prophage_name] = get_child_ends(prophage, parent_seq)
-
-    return ends
-
-
-def print_data(manual_data, testing_data, matches):
-    """
-    Prints the Testing Data
-    :param manual_data: Data from the manually annotated genome
-    :param testing_data: Data from testing software
-    """
-
-    print("\n-----------------------------------------------------------")
-    print("Prophage\tMatch\t\tEnds")
-    print("-----------------------------------------------------------\n")
-    for bac in manual_data:
-        print(bac)
-        print("Manually Annotated")
-        for prophage in manual_data[bac]:
-            print(f"{prophage}\t\t\t{manual_data[bac][prophage]}")
-
-        for child_dir in testing_data[bac]:
-            print("\n")
-            print(child_dir)
-
-            for prophage in testing_data[bac][child_dir]:
-                match = False
-                if prophage in matches[bac][child_dir]:
-                    match = True
-                print(f"{prophage}\t\t{match}"
-                      f"\t\t{testing_data[bac][child_dir][prophage]}")
-
-        print("-----------------------------------------------------------\n")
-
-
-def metrics(testing_data):
-    # true positive = binary classifier
-    # sn = (true_positive)/(true_positive+false_negative)
-    # ppv = (true_positive)/(true_positive+false_positive)
-    # for each software - calculate sensitivity, ppv
-    # for each, count the True/False of all prophages - total
-
-    metric_data = {"sn": {}, "ppv": {}}
-    print()
-
-
-def compile_data(main_dir):
-    if main_dir.is_dir() is False:
-        print("Invalid directory")
-        sys.exit(1)
-
-    testing_data = {}
-    manual_data = {}
-
-    for dir in sorted(main_dir.iterdir()):
-
-        # data = {}
-        # manual_coor = {}
-
-        name = dir.name
-        parent_data = get_parent_data(name, dir)
-        # parent_ends = parent_data[0]
-        parent_seq = parent_data[1]
-
-        # dictionary with all the data for each phage
-        testing_data[name] = {}
-        manual_data[name] = {}
-
-        # for each directory in the parent directory - need to get results
-        for child_dir in sorted(dir.iterdir()):
-            if child_dir.is_dir() is False:     # parent sequence FASTA file
-                continue
-
-            data_from = child_dir.name
-
-            if data_from == "Haley":
-                manual_coor = get_child_data(child_dir, parent_seq)
-                manual_data[name] = manual_coor
-            else:
-                data = get_child_data(child_dir, parent_seq)
-                testing_data[name][data_from] = data
-
-    # testing_data format
-    # {phage_name: {software_name: {prophage_name: <ends>, ...}, ...}, ...}
-    # manual_data format
-    # {phage_name: {prophage_name: <ends>, ...}, ...}
-
-    # compare here - add binary classifier to testing_data
-
-    # print(testing_data)
-    matches = comparison(manual_data, testing_data)
-
-    print_data(manual_data, testing_data, matches)
-
-    # print(matches)
+    return args
 
 
 def main():
-    compile_data(get_dir())    # all the prophages are inside this directory
+    """Run the testing module."""
+    # 3 args: reference path, manual path, software path
+    args = get_args()
+
+    comparison(args.dir, args.manual, args.software)
 
 
 if __name__ == '__main__':
