14d3bbfaeb2596eb3ec2450c911491c3970597c2
Christian Gauthier <chg60@pitt.edu>
2021-05-07 08:51:24 -0400

    removed redundant code

--- src/prophicient/functions/visualization.py
+++ src/prophicient/functions/visualization.py
@@ -34,24 +34,19 @@ def add_track_feature(feature_set, feature):
     if feature.type == "CDS":
         sigil = "ARROW"
         name = feature.qualifiers["product"][0]
-        if feature.location.strand == 1:
-            color, angle = colors.green, 0
-        else:
-            color, angle = colors.red, 180
+        if name == "hypothetical protein":
+            name = ""
     elif feature.type == "tRNA":
         sigil, color = "BOX", colors.blue
         name = feature.qualifiers["note"][0]
-        if feature.location.strand == 1:
-            angle = 45
-        else:
-            angle = 225
     else:
         sigil, color = "BOX", colors.gold
         name = feature.type
-        if feature.location.strand == 1:
-            angle = 0
-        else:
-            angle = 180
+
+    if feature.location.strand == 1:
+        color, angle = colors.green, 45
+    else:
+        color, angle = colors.red, 225
 
     feature_set.add_feature(feature, color=color, label=True, name=name,
                             labelsize=3, label_angle=angle, sigil=sigil,
