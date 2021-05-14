ca21faaa1cffcd192a1235741eb56f3ee273de13
ananya-venbakkam <ananya.venbakkam@gmail.com>
2021-05-14 16:19:08 -0400

    linear plot

--- interface/linear_plot.py
+++ interface/linear_plot.py
@@ -1,4 +1,7 @@
+"""BiopythonTrasnlator modified for use for Prophicient display."""
+
 from dna_features_viewer import BiopythonTranslator
+from dna_features_viewer import GraphicFeature
 
 
 class CustomTranslator(BiopythonTranslator):
@@ -29,7 +32,7 @@ class CustomTranslator(BiopythonTranslator):
 
     def compute_feature_linewidth(self, feature):
         """Compute the edge width of the feature's arrow/rectangle."""
-        return 5.0
+        return 10
 
     def compute_feature_label(self, feature):
         """Compute the label of the feature."""
@@ -51,8 +54,12 @@ class CustomTranslator(BiopythonTranslator):
         """Compute the feature legend text."""
         return feature.type
 
+    def compute_feature_box_color(self, feature):
+        """Compute a box_color for this feature."""
+        return "black"
+
     def compute_feature_color(self, feature):
-        """Compute color for the given feature."""
+        """Compute color for the given feature - differences based on type."""
         if feature.location.strand == 1:
             # green for forward
             CDS_color = "#449f66"
@@ -63,6 +70,7 @@ class CustomTranslator(BiopythonTranslator):
         color_dict = {"source": "#34ff56",
                       "gene": "#f45562",
                       "CDS": CDS_color,
+                      "misc_recomb": "#fbf3f6",
                       "tRNA": '#4554f4',
                       "tmRNA": '#56f443'}
 
