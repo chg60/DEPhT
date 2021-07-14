a982ea0a1d9250fb638aca79459217019379f5b7
Christian Gauthier <chg60@pitt.edu>
2021-07-14 10:43:51 -0400

    new model renders GaussianNB classifier obsolete

--- src/train_model/functions/model_training.py
+++ src/train_model/functions/model_training.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import KFold
-from sklearn.naive_bayes import GaussianNB
 
 from prophicient.classes.prophage_classifier import ProphageClassifier
 from prophicient.functions.statistics import average, mcc
@@ -40,7 +38,6 @@ def _mcc_score(real_classes, predict_classes):
     the mcc.
     """
     tps, tns, fps, fns = _score(real_classes, predict_classes)
-    print(tps, tns, fps, fns)
     return mcc(tps, fns, tns, fps)
 
 
@@ -83,67 +80,6 @@ def train_prophage_classifier(prophage_data, bacteria_data):
                            bacteria_data.iloc[:, :-1]), axis=0)
     all_labels = pd.concat((prophage_data.iloc[:, -1],
                             bacteria_data.iloc[:, -1]), axis=0)
-
-    clf.fit(all_feats, all_labels)
-
-    return clf, mean_mcc
-
-
-def train_bayes_classifier(prophage_data, bacteria_data):
-    """
-    Train a Naive Bayes classifier to descriminate between prophage
-    and bacterial genes.
-
-    :param prophage_data: prophage gene features
-    :type prophage_data: pandas.DataFrame
-    :param bacteria_data: bacterial gene features
-    :type bacteria_data: pandas.DataFrame
-    :return: best_model
-    """
-    # Cast pandas DataFrames to numpy Arrays
-    prophage_data = np.array(prophage_data)
-    bacteria_data = np.array(bacteria_data)
-
-    kf = KFold(n_splits=5, shuffle=True, random_state=42)
-
-    prophage_splits = [x for x in kf.split(prophage_data)]
-    bacteria_splits = [x for x in kf.split(bacteria_data)]
-
-    p_trains = [x[0] for x in prophage_splits]
-    p_tests = [x[1] for x in prophage_splits]
-    b_trains = [x[0] for x in bacteria_splits]
-    b_tests = [x[1] for x in bacteria_splits]
-
-    mcc_scores = list()
-
-    clf = GaussianNB()
-
-    zipper = zip(p_trains, p_tests, b_trains, b_tests)
-    for p_train, p_test, b_train, b_test in zipper:
-        train_feats = np.concatenate((prophage_data[p_train, :-1],
-                                      bacteria_data[b_train, :-1]), axis=0)
-        train_labels = np.concatenate((prophage_data[p_train, -1],
-                                       bacteria_data[b_train, -1]), axis=0)
-        test_feats = np.concatenate((prophage_data[p_test, :-1],
-                                     bacteria_data[b_test, :-1]), axis=0)
-        test_labels = np.concatenate((prophage_data[p_test, -1],
-                                      bacteria_data[b_test, -1]), axis=0)
-
-        ratio = float(len(b_train))/len(p_train)
-        sample_weights = [ratio] * len(p_train)
-        sample_weights.extend([1] * len(b_train))
-
-        clf.fit(train_feats, train_labels, sample_weight=sample_weights)
-
-        predictions = clf.predict(test_feats)
-        mcc_scores.append(_mcc_score(test_labels, predictions))
-
-    mean_mcc = average(mcc_scores)
-
-    all_feats = np.concatenate((prophage_data[:, :-1],
-                                bacteria_data[:, :-1]), axis=0)
-    all_labels = np.concatenate((prophage_data[:, -1],
-                                 bacteria_data[:, -1]), axis=0)
 
     clf.fit(all_feats, all_labels)
 
