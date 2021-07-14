266ff2c8e5df27dc581a0480b23a3cca5ad1d378
Christian Gauthier <chg60@pitt.edu>
2021-07-14 13:24:57 -0400

    re-structured; deleted obsolete code

--- test_pipeline.py
+++
@@ -1,9 +0,0 @@
-def sensitivity(true_positive, false_negative):
-    sn = (true_positive)/(true_positive+false_negative)
-    sn *= 100
-    return sn
-
-def psoitive_predictive_value(true_positive, false_positive):
-    ppv = (true_positive)/(true_positive+false_positive)
-    ppv *= 100
-    return ppv
